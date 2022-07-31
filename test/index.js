3840f11a76a6b16071731d0aea0220bc251f44c3
lkmoody@gmail.com <lkmoody@gmail.com>
2022-07-31 16:05:34 -0600

    Adding Mocha

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+
+describe('Array', () => {
+    describe('#indexof()', () => {
+        it('should return -1 when the value is not present', () => {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
